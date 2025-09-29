1b1e3edddb042e59d2a53743b83894f76521b638
hanissiddiq <hanissiddiq10@gmail.com>
2025-09-29 11:03:14 +0700

    Codelab 2 Modif Tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sisata/detail_screen.dart';
 
 void main() {
 runApp(const MyApp());
@@ -28,9 +29,12 @@ const DetailScreen({super.key});
   @override
   Widget build(BuildContext context) {
   return Scaffold(
-    body: SafeArea(child: Column(
+    body: SafeArea(
+      child: SingleChildScrollView(
+      child: Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
+        Image.asset('images/farm-house.jpg'),
         Container(
           margin: const EdgeInsets.only(top: 16.0),
           child:  Text("Pantai Pasir Putih Aceh", textAlign: TextAlign.center,style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
@@ -75,10 +79,28 @@ const DetailScreen({super.key});
               style: TextStyle(fontSize: 16.0),
             ),
           ),
+
+         SizedBox(
+                height: 200,
+                child: ListView( scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(padding: const EdgeInsets.all(4.0),child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),),
+                    Padding(padding: const EdgeInsets.all(4.0),child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),),
+                    Padding(padding: const EdgeInsets.all(4.0),child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),),
+                  ],
+                ),
+              ),
+
+          // Image.network(
+          //   'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+          
         
       ],
     ),
-    )
+    ))
   );
   }
 }
