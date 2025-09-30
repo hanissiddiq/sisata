b7efb7c0053c8b3d6dfc9fa989e817bc5bfd1c3b
hanissiddiq <hanissiddiq10@gmail.com>
2025-09-30 10:01:37 +0700

    codelab 3 modifikasi tampilan bisa navigate page

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sisata/model/tourism_place.dart';
+class DetailScreen extends StatelessWidget {
+  final TourismPlace place;
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your actual widget tree
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.name),
+      ),
+      body: Center(
+        child: Text('Detail Screen for ${place.name}'),
+      ),
+    );
+  }
+}
