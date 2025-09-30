78eb480bd4e7fccf3dbe3ab9d80fe987e85864c4
hanissiddiq <hanissiddiq10@gmail.com>
2025-09-30 10:08:58 +0700

    Modifikasi Code to clean Code and add state back & favorite button

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,16 +1,15 @@
-
 import 'package:flutter/material.dart';
-import 'package:sisata/main.dart';
+import 'package:sisata/detail_screen.dart';
 import 'package:sisata/model/tourism_place.dart';
- 
+
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wisata Bandung'),
+        title: const Text('Wisata Bandung'),
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
@@ -18,7 +17,7 @@ class MainScreen extends StatelessWidget {
           return InkWell(
             onTap: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen();
+                return DetailScreen(place: place);
               }));
             },
             child: Card(
@@ -58,60 +57,3 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
-  // const MainScreen({super.key});
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wisata Medan'),
-        ),
-        body: ListView.builder(
-        itemBuilder: (context, index) {
-          final TourismPlace place = tourismPlaceList[index];
-       return InkWell(
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-    return DetailScreen();
-          }));
-          },
-          child: Card(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            
-                  Expanded(
-                        flex: 1,
-                        child: Image.asset('images/farm-house.jpg'),
-                      ),
-                 Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            'Farm House Lembang',
-                            style: const TextStyle(fontSize: 16.0),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text('Lembang'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-   
-        );
-         
-        itemCount: tourismPlaceList.length;
-        }
-    ),
-    );
-  }
